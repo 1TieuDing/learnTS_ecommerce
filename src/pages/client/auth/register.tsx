041de8f6b67d3490558fca6bd7502f80e52ae982
1TieuDing <vtuananhh@gmail.com>
2025-06-29 20:50:26 +0700

    v7 + register 2

--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,9 +1,9 @@
-import { Button, Divider, Form, Input } from 'antd';
+import { App, Button, Divider, Form, Input } from 'antd';
 import type { FormProps } from 'antd';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './register.scss'
-import { loginAPI } from '@/services/api';
+import { registerAPI } from '@/services/api';
 
 type FieldType = {
     fullName: string;
@@ -14,12 +14,24 @@ type FieldType = {
 
 const RegisterPage = () => {
     const [isSubmit, setIsSubmit] = useState(false)
+    const { message } = App.useApp()
+    const navigate = useNavigate()
 
     const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
-        console.log(values)
+        setIsSubmit(true)
 
-        const res = await loginAPI("admin@gmail.com", "123456")
-        console.log(">>> check res: ", res)
+        const { email, fullName, password, phone } = values
+
+        const res = await registerAPI(fullName, email, password, phone)
+        if (res.data) {
+            // success
+            message.success("Đăng ký user thành công.")
+            navigate("/login")
+        } else {
+            // error
+            message.error(res.message)
+        }
+        setIsSubmit(false)
     };
 
 
