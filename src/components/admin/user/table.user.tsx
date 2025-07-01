3676c6366330003546aea7ff499dc4f406b93932
1TieuDing <vtuananhh@gmail.com>
2025-07-01 08:48:11 +0700

    v17 + view detail

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -5,66 +5,8 @@ import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { ProTable } from '@ant-design/pro-components';
 import { Button } from 'antd';
 import { useRef, useState } from 'react';
+import DetailUser from './detail.user';
 
-
-
-const columns: ProColumns<IUserTable>[] = [
-    {
-        dataIndex: 'index',
-        valueType: 'indexBorder',
-        width: 48,
-    },
-    {
-        title: 'Id',
-        dataIndex: '_id',
-        hideInSearch: true,
-        render(dom, entity, index, action, schema) {
-            return (
-                <a href="#">{entity._id}</a>
-            )
-        },
-    },
-    {
-        title: 'Full Name',
-        dataIndex: 'fullName',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        copyable: true
-    },
-    {
-        title: 'Created At',
-        dataIndex: 'createdAt',
-        valueType: 'date',
-        sorter: true,
-        hideInSearch: true
-    },
-    {
-        title: 'Created At',
-        dataIndex: 'createdAtRange',
-        valueType: 'dateRange',
-        hideInTable: true
-    },
-    {
-        title: 'Action',
-        hideInSearch: true,
-        render(dom, entity, index, action, schema) {
-            return (
-                <>
-                    <EditTwoTone
-                        twoToneColor="#f57800"
-                        style={{ cursor: "pointer", marginRight: 15 }}
-                    />
-                    <DeleteTwoTone
-                        twoToneColor="#ff4d4f"
-                        style={{ cursor: "pointer" }}
-                    />
-                </>
-            )
-        },
-    }
-];
 
 const TableUser = () => {
     const actionRef = useRef<ActionType>();
@@ -75,6 +17,76 @@ const TableUser = () => {
         pages: 0,
         total: 0
     })
+
+    const [openViewDetail, setOpenViewDetail] = useState<boolean>(false)
+    const [dataViewDetail, setDataViewDetail] = useState<IUserTable | null>(null)
+
+    const columns: ProColumns<IUserTable>[] = [
+        {
+            dataIndex: 'index',
+            valueType: 'indexBorder',
+            width: 48,
+        },
+        {
+            title: 'Id',
+            dataIndex: '_id',
+            hideInSearch: true,
+            render(dom, entity, index, action, schema) {
+                return (
+                    <a
+                        onClick={() => {
+                            setDataViewDetail(entity)
+                            setOpenViewDetail(true)
+                        }}
+                        href="#"
+                    >
+                        {entity._id}
+                    </a>
+                )
+            },
+        },
+        {
+            title: 'Full Name',
+            dataIndex: 'fullName',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            copyable: true
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'createdAt',
+            valueType: 'date',
+            sorter: true,
+            hideInSearch: true
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'createdAtRange',
+            valueType: 'dateRange',
+            hideInTable: true
+        },
+        {
+            title: 'Action',
+            hideInSearch: true,
+            render(dom, entity, index, action, schema) {
+                return (
+                    <>
+                        <EditTwoTone
+                            twoToneColor="#f57800"
+                            style={{ cursor: "pointer", marginRight: 15 }}
+                        />
+                        <DeleteTwoTone
+                            twoToneColor="#ff4d4f"
+                            style={{ cursor: "pointer" }}
+                        />
+                    </>
+                )
+            },
+        }
+    ];
+
 
     return (
         <>
@@ -145,6 +157,13 @@ const TableUser = () => {
                     </Button>
 
                 ]}
+            />
+
+            <DetailUser
+                openViewDetail={openViewDetail}
+                setOpenViewDetail={setOpenViewDetail}
+                dataViewDetail={dataViewDetail}
+                setDataViewDetail={setDataViewDetail}
             />
         </>
     );
