a8ee8d358e5fb9f38dfa83293380fee6828f104d
1TieuDing <vtuananhh@gmail.com>
2025-07-03 17:41:08 +0700

    v42 + loading skeleton

--- src/services/api.ts
+++ src/services/api.ts
@@ -117,7 +117,7 @@ export const getBookByIdAPI = (_id: string) => {
     const urlBackend = `/api/v1/book/${_id}`;
     return axios.get<IBackendRes<IBookTable>>(urlBackend, {
         headers: {
-            delay: 1000
+            delay: 3000
         }
     })
 }
