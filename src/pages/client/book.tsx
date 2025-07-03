f1cc3b77bb69bd8f0297548d01348d6e33c19b93
1TieuDing <vtuananhh@gmail.com>
2025-07-03 15:24:35 +0700

    v40 + deail book

--- src/pages/client/book.tsx
+++ src/pages/client/book.tsx
@@ -1,8 +1,20 @@
+/* eslint-disable prefer-const */
+import BookDetail from "@/components/client/book/book.detail"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
 
 const BookPage = () => {
+    let { id } = useParams()
+
+    useEffect(() => {
+        if (id) {
+            console.log("book id = ", id)
+        }
+    })
+
     return (
         <div>
-            Book page
+            <BookDetail />
         </div>
     )
 }
