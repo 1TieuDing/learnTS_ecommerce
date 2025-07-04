6ae899761fa9cedd7e60f1c27b2bb3d95113f41a
1TieuDing <vtuananhh@gmail.com>
2025-07-04 20:19:15 +0700

    v50 + order history

--- src/pages/client/order.tsx
+++ src/pages/client/order.tsx
@@ -47,7 +47,9 @@ const OrderPage = () => {
                             </Button>,
 
                             <Button key="history">
-                                Lịch sử mua hàng
+                                <Link to={"/history"} type="primary">
+                                    Lịch sử mua hàng
+                                </Link>
                             </Button>,
                         ]}
                     />
